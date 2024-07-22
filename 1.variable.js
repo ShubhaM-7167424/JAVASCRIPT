f4a22080dbebe1351c348b8e865ce86335fce562
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-07-22 17:52:44 +0530

    variable

---
+++ 1.variable.js
@@ -0,0 +1,11 @@
+//? variable name must begin with a letter, underscore or $
+//? variable name cannot contain spaces
+//? variable name cannot start with a number
+//? variable name can only contain letters, numbers, underscores and dollar signs
+//? variable name are case sensitive
+//? variable name cannot be a keyword
+//? variable name cannot be a reserved keyword
+
+var firstName = 'John';
+
+var a = 10;
