a6c554cf8c2965a66bcef9a041dab42b525704eb
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-07-25 09:04:49 +0530

    data types

---
+++ 3.data-types.js
@@ -0,0 +1,49 @@
+//* Primitive Data type
+//* can store single value
+
+//? number
+let age = 24;
+
+//? string
+let firstName = "Shubham";
+
+//? boolean
+let isStudent = true;
+console.log(5<10);
+
+//? null
+//? intentional absence of values 
+let result = null;
+
+//? undefined
+//? declared but not assigned any value
+let x;
+
+//? bigInt
+let bigNumber = 12345678901234567890123456789012345678901234567890n;
+
+//? symbol
+let id = Symbol("id");
+
+// ===================================================================
+
+//* Non-Primitive data types
+//* can store multiple values
+
+//? string
+
+//? object
+let person = {
+  name: "Shubham",
+  age: 28,
+  rollno: 24,
+};
+
+//? array
+let colors = ["red", "green", "blue"];
+
+//? function
+function greet() {
+  console.log("Hello, World!");
+  console.log('hi');
+}
