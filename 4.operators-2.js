4a43d405963d65d0a735a1f44bd4361cec7aee0e
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-07-27 07:54:17 +0530

    operators 2

---
+++ 4.operators-2.js
@@ -0,0 +1,26 @@
+//? Assignment operator
+
+let a = 10;
+a+=5 // a = a + 5
+a-=5 // a = a - 5
+a*=5 // a = a * 5
+a/=5 // a = a / 5
+a%=5 // a = a % 5 
+
+//? logical operator
+
+console.log((10>5) && (4<7));
+
+console.log((10<5) || (7>4));
+
+console.log(!true);
+
+//? conditional operator
+
+let age=25
+
+console.log(age>18 ? 'Adult' : 'Minor');
+
+age = 16
+
+console.log(age>18 ? 'Adult' : 'Minor');
