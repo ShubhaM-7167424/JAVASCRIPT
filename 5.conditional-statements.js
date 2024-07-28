bad775635de74a3327ee8dae5d7868fe7805c114
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-07-28 10:02:40 +0530

    conditional statements

---
+++ 5.conditional-statements.js
@@ -0,0 +1,15 @@
+// if statement
+
+let marks = 25
+
+if(marks > 30){
+    console.log('good marks');
+}
+
+else if(marks < 15){
+    console.log('fail');
+}
+
+else{
+    console.log('you need to work hard');
+}
