e860ffbd9029577f326cd7d3e2683b6556b6869e
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-07-29 10:37:14 +0530

    loops

---
+++ 6.loops.js
@@ -0,0 +1,21 @@
+// for loop
+
+for(let i=0; i<10; i++){
+    console.log('shubham');
+}
+
+// while loop
+
+let i=0;
+while (i<10) {
+    console.log(i);
+    i++;
+}
+
+// do while loop 
+
+let j=0;
+do{
+    console.log('hello');
+    j++;
+}while(j<0);
