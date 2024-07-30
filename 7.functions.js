3866f03a08b3865cd35414a5af1bd70c43dc602f
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-07-30 08:39:14 +0530

    functions

---
+++ 7.functions.js
@@ -0,0 +1,17 @@
+// functions 
+
+function sayHello(name) {
+    return 'Hello ' + name;
+}
+
+console.log(sayHello('shubham'));
+console.log(sayHello('vijay'));
+console.log(sayHello('sachin'));
+
+
+function sum(a, b) {       // parameters
+    return a+b;
+}
+
+console.log(sum(10, 5));  // arguments
+console.log(sum(20, 40));
