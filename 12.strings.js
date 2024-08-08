3f9de1663042bbf7953342295f93f75607f63c41
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-08-08 19:34:33 +0530

    strings

---
+++ 12.strings.js
@@ -0,0 +1,20 @@
+//! Strings in javascripts
+
+
+let a = 'hello'
+
+console.log(a);
+console.log(typeof a);
+
+let b = new String('hey')
+console.log(b);
+console.log(typeof b);
+
+//*=================================================================
+
+//! string literals 
+
+let fname = 'shubham'
+let lname = 'singh'
+
+console.log(`my name is ${fname} ${lname}`);
