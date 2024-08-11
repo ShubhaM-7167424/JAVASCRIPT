c1e19a9286de96f39b3f4b707d747d29cbdaead1
Shubham Singh <shubhamsingh7167424@gmail.com>
2024-08-11 09:56:50 +0530

    window objects

---
+++ 14.window-object.js
@@ -0,0 +1,11 @@
+//? window object in js 
+
+// it represents the browser window in which your code is running and it is created by browser itself
+
+// window is a global object
+
+// window is the object of the browser , it is not the object of the javascript
+
+// it has a lot of methods alert(), prompt(), confirm()
+
+ 
